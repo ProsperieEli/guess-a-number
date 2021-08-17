5fa123a669f77c72c51e79e813b9b682f9f26a23
ProsperieEli <ElijahProsperie@gmail.com>
2021-08-17 18:39:37 -0400

    first attempt

--- README.md
+++ README.md
@@ -9,3 +9,13 @@
 8) Consider your data model. What objects will you be using? What are the key/value pairs? What arrays do you need? What needs to live in local storage?
 9) Consider what features _depend_ on what other features. Use this dependency logic to figure out what order to complete tasks.
 
+
+
+## HTML
+    -2 buttons
+        -One for guessing, another for resetting
+    -displays
+        -one for results
+        -one for guesses
+        -one for hints
+    
--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 // import functions and grab DOM elements
-
+const myButton = document.getElementById('buttonEl'); 
+const myInput = document.getElementById('guess');
+const endDisplay = document.getElementById('display-results');
+//const restart = document.getElementById('reset');
 // initialize global state
-
+let correctAnswer = 21;
+const hint = document.getElementById('hint');
+let chance =4;
+const chanceText = document.getElementById('chances');
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+  myButton.addEventListener('click', () => {
+    let userGuess = Number(myInput.value);
+    console.log(userGuess);
+    if (correctAnswer === userGuess){
+      endDisplay.textContent = 'You Win!';
+    }else if (correctAnswer !== userGuess && chance === 4) {
+     
+      
+    hint.textContent = 'I am a legal age in America.';
+    chanceText.textContent = `You have ${chance} remaining.`
+    chance--;
+    } else if (correctAnswer !== userGuess && chance ===3){  
+     hint.textContent = 'I was born in the 1990s';
+     chanceText.textContent = `You have ${chance} remaining.`;
+     chance--;
+    }
+
+    
+  })
